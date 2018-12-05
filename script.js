162afb72b2665c9e6d91c82dc0fa9355015d26e3
Khashayar Farhadi <kashfarhadi@gmail.com>
2018-12-05 16:48:19 -0500

    uploaded first git int and files

---
+++ script.js
@@ -0,0 +1,10 @@
+// 1. Reverse a string
+// 2. Reverse a sentence ("bob likes dogs" -> "dogs likes bob")
+// 3. Find the minimum value in an array
+// 4. Find the maximum value in an array
+// 5. Calculate a remainder (given a numerator and denominator)
+// 6. Return distinct values from a list including duplicates (i.e. "1 3 5 3 7 3 1 1 5" -> "1 3 5 7")
+// 7. Return distinct values and their counts (i.e. the list above becomes "1(3) 3(3) 5(2) 7(1)")
+
+// 8. Given a string of expressions (only variables, +, and -) and an object describing a set of variable/value 
+// 8 cont. pairs like {a: 1, b: 7, c: 3, d: 14}, return the result of the expression ("a + b+c -d" would be -3).
